878aac6a3c96f1bcd1cfbffe94b3aad0503de3ec
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-09-08 10:10:20 +0200

    :rocket: Add the default register api route
    Signed-off-by: loicmaes <97891718+loicmaes@users.noreply.github.com>

--- server/api/portal/auth/register.post.ts
+++ server/api/portal/auth/register.post.ts
@@ -1,6 +1,10 @@
 import type { CreateUserBody } from "~/types/user";
+import { generateCode } from "~/lib/number";
 
 export default defineEventHandler(async (event) => {
   const body = await readBody<CreateUserBody>(event);
   console.log(body);
+  return generateCode({
+    numbers: true,
+  });
 });
