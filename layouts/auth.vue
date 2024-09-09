ce303536a6a8e2aef070314110b91a3e418bb552
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-09-09 12:40:28 +0200

    :rocket: Add auth layout
    Signed-off-by: loicmaes <97891718+loicmaes@users.noreply.github.com>

---
+++ layouts/auth.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="w-full min-h-dvh grid place-items-center">
+    <div class="w-full max-w-xl">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
