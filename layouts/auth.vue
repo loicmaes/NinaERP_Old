f4cdc512d34b8ff76b9d77860f50218d2d1c6f43
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-09-10 09:57:19 +0200

    :lipstick: mobile behavior
    Signed-off-by: loicmaes <97891718+loicmaes@users.noreply.github.com>

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="w-full min-h-dvh grid place-items-center">
+  <div class="w-full min-h-dvh grid place-items-center px-6">
     <div class="w-full max-w-xl">
       <slot />
     </div>
