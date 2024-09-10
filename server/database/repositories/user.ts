a16cbfade2e5f8f51a8e61d99668aee2f1acd627
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-09-10 09:56:55 +0200

    :rocket: Handle unique constraint on user
    Signed-off-by: loicmaes <97891718+loicmaes@users.noreply.github.com>

--- server/database/repositories/user.ts
+++ server/database/repositories/user.ts
@@ -1,7 +1,9 @@
+import { Prisma } from "@prisma/client";
 import type { CodeGeneratorOptions } from "~/lib/number";
 import { generateCode } from "~/lib/number";
 import prisma from "~/server/database/client";
 import type { CreateUserBody, RichUser } from "~/types/user";
+import { UniqueConstraintError } from "~/types/error";
 
 const verificationCodeOptions: CodeGeneratorOptions = {
   length: 32,
@@ -27,4 +29,33 @@ export async function createUser(body: CreateUserBody): Promise<RichUser> {
       userInfo: true,
     },
   }) as RichUser;
+  try {
+    return await prisma.user.create({
+      data: {
+        ...body,
+        verificationCode,
+        userInfo: {
+          create: {
+            ...body.userInfo,
+            contactEmail: body.userInfo.contactEmail ?? body.email,
+          },
+        },
+      },
+      include: {
+        userInfo: true,
+      },
+    }) as RichUser;
+  }
+  catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      switch (e.code) {
+        case "P2002":
+          throw new UniqueConstraintError();
+        default:
+          throw e;
+      }
+    }
+
+    throw e;
+  }
 }
