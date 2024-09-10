7f071c1815a2755867b357accb687e584c6c3f42
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-09-10 09:57:48 +0200

    :rocket: Handle unique constraint
    Signed-off-by: loicmaes <97891718+loicmaes@users.noreply.github.com>

--- server/services/auth.ts
+++ server/services/auth.ts
@@ -3,24 +3,38 @@ import type { CreateUserBody, RichUser, User } from "~/types/user";
 import { createUser } from "~/server/database/repositories/user";
 import type { AuthSession, CreateAuthSessionBody } from "~/types/authSession";
 import { createAuthSession } from "~/server/database/repositories/authSession";
+import { UniqueConstraintError } from "~/types/error";
 
 export const authTokenCookie = "auth-token";
 export const userUidCookie = "user-uid";
 
-export async function register(event: H3Event<Request>, userData: CreateUserBody): Promise<User> {
-  const user: RichUser = await createUser(userData);
-  const code = user.verificationCode;
+export async function register(event: H3Event<Request>, userData: CreateUserBody): Promise<User | undefined> {
+  try {
+    const user: RichUser = await createUser(userData);
+    const code = user.verificationCode;
 
-  console.log(code);
-  // TODO: send mail with otp code
+    console.log(code);
+    // TODO: send mail with otp code
 
-  await makeSession(event, {
-    userUid: user.uid,
-  });
+    await makeSession(event, {
+      userUid: user.uid,
+    });
 
-  return {
-    ...user,
-  };
+    return {
+      ...user,
+    };
+  }
+  catch (e) {
+    if (e instanceof UniqueConstraintError) sendError(event, createError({
+      statusCode: 409,
+      statusMessage: "Le login ou l'e-mail est déjà utilisé !",
+    }));
+    sendError(event, createError({
+      statusCode: 500,
+      statusMessage: "Une erreur est survenue !",
+    }));
+    return;
+  }
 }
 
 export async function makeSession(event: H3Event<Request>, body: CreateAuthSessionBody): Promise<AuthSession> {
